8cddfff31014c361dfa5c1ac03611d5d16a92185
Wes-White <wesleyrwhite86@gmail.com>
2019-12-04 15:17:25 -1000

    Set up initial test for display, Display is being rendered

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("The Display renders without crashing", () => {
+  render(<Display />);
+});
