01f4c6779ea6c6f4b7163ce1e9d6ea05e8c37bb4
Wes-White <wesleyrwhite86@gmail.com>
2019-12-04 16:09:57 -1000

    Display testing completed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,3 +8,26 @@ import Display from "./Display";
 test("The Display renders without crashing", () => {
   render(<Display />);
 });
+
+test("Should match snapshot", () => {
+  expect(render(<Display />)).toMatchSnapshot();
+});
+
+test("We are unlocked and opened", () => {
+  const { getByText } = render(<Display />);
+
+  const open = getByText(/open/i);
+  const unlocked = getByText(/unlocked/i);
+
+  expect(open.className).toMatch(/green-led/i);
+  expect(unlocked.className).toMatch(/green-led/i);
+});
+
+test("We are locked and closed", () => {
+  const { getByText } = render(<Display closed={true} locked={true} />);
+
+  const closed = getByText(/closed/i);
+  const locked = getByText(/locked/i);
+  expect(closed.className).toMatch(/red-led/i);
+  expect(locked.className).toMatch(/red-led/i);
+});
